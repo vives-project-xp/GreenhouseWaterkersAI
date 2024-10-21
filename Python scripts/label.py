00ad584b689def0be69b9263313250b7e58f7317
LynnDelaere <lynn.delaere@hotmail.be>
2024-10-21 17:50:22 +0200

    Export labeled data to JSON file

--- Python scripts/label.py
+++ Python scripts/label.py
@@ -1,13 +1,86 @@
-import labelbox
+import labelbox as lb
+import urllib.request
+from PIL import Image
+import time
+import json
 
-LB_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbTIzMDl0bTAwNHU0MDd5NzZxYWNmOXJyIiwib3JnYW5pemF0aW9uSWQiOiJjbTIzMDl0bGowNHUzMDd5N2hzZXI4OXYxIiwiYXBpS2V5SWQiOiJjbTIzOHJvOWcxOHI2MDd4MzkxN3RjNHYyIiwic2VjcmV0IjoiZTFjYTljZDdlOGJkOWIxZjkwZWZiZThiYTRkN2MxZWMiLCJpYXQiOjE3Mjg1NjEzMDEsImV4cCI6MjM1OTcxMzMwMX0.-vNSCSPtMVL2a8XSNrY1af67brl93Cbf_WCkZgH1OSg'
+# Creat a new client
+API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbTIzMDl0bTAwNHU0MDd5NzZxYWNmOXJyIiwib3JnYW5pemF0aW9uSWQiOiJjbTIzMDl0bGowNHUzMDd5N2hzZXI4OXYxIiwiYXBpS2V5SWQiOiJjbTJkY2xsZ2UwZGc5MDd3bzJiOTBoN2kyIiwic2VjcmV0IjoiZjdhOGVkZjRmYWVmNGExYTlkZmE3NWFhOGIzYzM5NWIiLCJpYXQiOjE3MjkxNzIzOTgsImV4cCI6MjM2MDMyNDM5OH0.SMpeYzwJRLb5lJLGZ6JnOzbGpl-eumI0nHhNT7IHWAM'
+client = lb.Client(API_KEY)
 
-client = labelbox.Client(api_key = LB_API_KEY)
-export_task = labelbox.ExportTask.get_task(client, "cm238ctbc00u707vd2e940gpe")
+# Insert the project id
+PROJECT_ID = 'cm23kaijl09t107ymbvqjgqx4'
+project = client.get_project(PROJECT_ID)
 
-converter = labelbox.FileConverter(file_path="export-output.json")
-export_task.get_stream(converter=converter).start()
+# Export params to include/export certain fields
+export_params = {
+    "attachments": True,
+    "metadata_fields": True,
+    # "embeddings": True,
+    "data_row_details": True,
+    "project_details": True,
+    "label_details": True,
+    "performance_details": True,
+    # "interpolated_frames": True,    
+}
 
-def json_stream_handler(output: labelbox.JsonConverterOutput):
-  print(output.json_str)
-export_task.get_stream().start(stream_handler=json_stream_handler)
+# Filters to filter the data rows
+# Note: Filters follow AND logic
+filters = {
+    # "last_activity_at": ["2000-01-01 00:00:00", "2050-01-01 00:00:00"],
+    # "label_created_at": ["2000-01-01 00:00:00", "2050-01-01 00:00:00"],
+    # "global_keys": ["<global_key>", "<global_key>"],
+    # "data_row_ids": ["<data_row_id>", "<data_row_id>"],
+    # "batch_ids": ["<batch_id>", "<batch_id>"],
+    "workflow_status": "Done"
+}
+
+# Export the data rows
+export_task = project.export_v2(params=export_params, filters=filters)
+export_task.wait_until_done()
+
+if export_task.errors:
+    print(export_task.errors)
+
+export_json = export_task.result
+print("Result: ", export_json)
+
+# Function to handle the JSON stream
+def json_stream_handler(output: lb.BufferedJsonConverterOutput):
+    print(output.json)
+
+
+if export_task.has_errors():
+    export_task.get_buffered_stream(stream_type=lb.StreamType.ERRORS).start(
+        stream_handler=lambda error: print(error))
+
+if export_task.has_result():
+    export_json = export_task.get_buffered_stream(
+        stream_type=lb.StreamType.RESULT).start(
+            stream_handler=json_stream_handler)
+
+print(
+    "file size: ",
+    export_task.get_total_file_size(stream_type=lb.StreamType.RESULT),
+)
+print(
+    "line count: ",
+    export_task.get_total_lines(stream_type=lb.StreamType.RESULT),
+)
+
+# Convert the JSON to a dictionary
+if export_task.errors:
+    with open('Python scripts/JSONFileErrors.txt', 'w') as error_file:
+        json.dump(export_task.errors, error_file, indent=4)
+    print("Errors saved to errors.txt")
+else:
+    print("No errors found.")
+
+# Check if the export task has results and save them to result.txt
+if export_task.has_result():
+    export_json = export_task.result
+    with open('Python scripts/JSONFileResults.txt', 'w') as result_file:
+        json.dump(export_json, result_file, indent=4)
+    print("Results saved to result.txt")
+else:
+    print("No results found.")
